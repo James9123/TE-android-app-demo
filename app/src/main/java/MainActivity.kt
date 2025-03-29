802371ce1e126db97d28a9252016860c99707e3d
James9123 <jamesbryanwagner@gmail.com>
2025-03-29 15:10:25 -0600

    Initial commit

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.highcapable.androidappdemo
+
+import android.os.Bundle
+import com.highcapable.betterandroid.ui.component.activity.AppBindingActivity
+import com.highcapable.androidappdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppBindingActivity<ActivityMainBinding>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO Start your coding here.
+    }
+}
